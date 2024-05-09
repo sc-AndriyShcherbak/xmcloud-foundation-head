3a272b4d4827bc3bfd570fd373715fee2ec4ca2b
Andriy Shcherbak <52037942+sc-AndriyShcherbak@users.noreply.github.com>
2024-05-09 11:08:59 +0300

    Add ItemUrlBuilder

---
+++ src/platform/XmCloudnov.Extensions.NOVItemUrlBuilder.cs
@@ -0,0 +1,24 @@
+// XmCloudnov.Extensions.NOVItemUrlBuilder
+using System;
+using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
+
+public class NOVItemUrlBuilder : ItemUrlBuilder
+{
+	public NOVItemUrlBuilder(DefaultItemUrlBuilderOptions defaultOptions)
+		: base(defaultOptions)
+	{
+	}
+
+	public override string Build(Item item, ItemUrlBuilderOptions options)
+	{
+		string text = base.Build(item, options);
+		if (text != null && text.ToLower().Contains("/products/"))
+		{
+			int num = text.LastIndexOf('/') + 1;
+			string text2 = text.Substring(num, text.Length - num);
+			text = text.ToLower().Split(new string[1] { "/products/" }, StringSplitOptions.None)[0] + "/products/" + text2?.ToLower();
+		}
+		return text?.ToLower();
+	}
+}
