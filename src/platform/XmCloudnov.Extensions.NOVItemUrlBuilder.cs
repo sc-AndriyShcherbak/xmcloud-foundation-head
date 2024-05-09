43630a60c6a283778ed477d62f3155437e05a7c2
Andriy Shcherbak <52037942+sc-AndriyShcherbak@users.noreply.github.com>
2024-05-09 11:32:25 +0300

    Update namespace

--- src/platform/XmCloudnov.Extensions.NOVItemUrlBuilder.cs
+++ src/platform/XmCloudnov.Extensions.NOVItemUrlBuilder.cs
@@ -3,22 +3,25 @@ using System;
 using Sitecore.Data.Items;
 using Sitecore.Links.UrlBuilders;
 
-public class NOVItemUrlBuilder : ItemUrlBuilder
+namespace XmCloudSXAStarter
 {
-	public NOVItemUrlBuilder(DefaultItemUrlBuilderOptions defaultOptions)
-		: base(defaultOptions)
+	public class NOVItemUrlBuilder : ItemUrlBuilder
 	{
-	}
-
-	public override string Build(Item item, ItemUrlBuilderOptions options)
-	{
-		string text = base.Build(item, options);
-		if (text != null && text.ToLower().Contains("/products/"))
+		public NOVItemUrlBuilder(DefaultItemUrlBuilderOptions defaultOptions)
+			: base(defaultOptions)
 		{
-			int num = text.LastIndexOf('/') + 1;
-			string text2 = text.Substring(num, text.Length - num);
-			text = text.ToLower().Split(new string[1] { "/products/" }, StringSplitOptions.None)[0] + "/products/" + text2?.ToLower();
 		}
-		return text?.ToLower();
+
+		public override string Build(Item item, ItemUrlBuilderOptions options)
+		{
+			string text = base.Build(item, options);
+			if (text != null && text.ToLower().Contains("/products/"))
+			{
+				int num = text.LastIndexOf('/') + 1;
+				string text2 = text.Substring(num, text.Length - num);
+				text = text.ToLower().Split(new string[1] { "/products/" }, StringSplitOptions.None)[0] + "/products/" + text2?.ToLower();
+			}
+			return text?.ToLower();
+		}
 	}
 }
